13221608b66c9a8017c5da70f415910e39b6a026
Lucas da Silva <58397963+lucxsapollo@users.noreply.github.com>
2023-01-02 17:01:17 -0300

    Exercício pronto

---
+++ exe_one.py
@@ -0,0 +1,20 @@
+# VARIAVEIS
+nome = input('Qual seu nome?')
+
+cpf = input('Qual seu CPF?')
+
+endereço = input('Qual seu endereço?')
+
+idade = input('Qual sua idade?')
+
+altura = input('Qual sua altura?')
+
+telefone = input('Qual seu numero de telefone?')
+
+# PRINT
+print('Nome:', nome)
+print('CPF:', cpf)
+print('Endereço:', endereço)
+print('Idade:', idade, 'anos')
+print('Altura:', altura, 'de altura')
+print('Telefone:', telefone)
